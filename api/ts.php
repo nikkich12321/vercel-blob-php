fff87e20d138c27de35355d0d3d5d1bc2f597c11
nikkich12321 <110050286+nikkich12321@users.noreply.github.com>
2024-09-03 22:54:49 +0530

    update

--- api/ts.php
+++ api/ts.php
@@ -1,15 +1,10 @@
 <?php
 $data=$_REQUEST['data'];
-$id=$_REQUEST['id'];
-$url=base64_decode($data);
-$url_dump=explode(':80/live/shoaib/shoaib/'.$id.'.m3u8',$url);
-$url_base=$url_dump[0];
-#echo $url_base;
 $ch = curl_init();
     $headers = array(
 'user-agent: '.base64_encode(time()),
     );
-    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_URL, 'http://'.$data);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_HEADER, 0);
 #	curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
@@ -23,8 +18,6 @@ $ch = curl_init();
     // Timeout in seconds
     curl_setopt($ch, CURLOPT_TIMEOUT, 30);
     $json=curl_exec($ch);
-	$dump=str_replace('/hls',$url_base.'/hls',$json);
-	$dump_final=str_replace('http://','https://vercel-blob-php-sigma.vercel.app/api/ts.php?data=',$dump);
-	echo $dump_final;
+	echo $json;
 
 ?>
